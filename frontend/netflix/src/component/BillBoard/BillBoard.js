360702f9244bdb4c034e893a5c27dd2bbb972577
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-01 00:51:07 +0300

    completed billboard

---
+++ frontend/netflix/src/component/BillBoard/BillBoard.js
@@ -0,0 +1,35 @@
+import React from "react"
+import styles from './BillBoard.module.css'
+import billboardHeroImage from './assets/Arisu.webp'
+import billboardHeroTitle from './assets/Title.webp'
+
+function BillBoard (){
+    return(
+        <div className={styles.billboard}>
+            <div className={styles.innerContainer}>
+                <div className={styles.imageWrapper}>
+                    <img className={styles.heroImage} src={billboardHeroImage} alt="Arisu in the middle of Tokyo which has skyscrapers covered in overgrown plants" />
+                    <div className={styles.billboard__fadeBottom}/>
+                </div>
+                <div className={styles.info}>
+                    <img src={billboardHeroTitle} alt="Alice in Boderland" />
+                    <div className={styles.description}>
+                        An aimless gamer and his two friends find themselves in a paralle Tokyo, where they're forced to compete in a series of sadistic games to survive.
+                    </div>
+                    <div className={styles. buttonLinks}>
+                        <a className='btn btn-light btn-lg'> 
+                        <i className="icon play" />
+                        <span>Play</span>               
+                        </a>
+                        <button className='btn btn-secondary btn-lg'>
+                        <i className="icon info circle" />
+                        More Info
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BillBoard
