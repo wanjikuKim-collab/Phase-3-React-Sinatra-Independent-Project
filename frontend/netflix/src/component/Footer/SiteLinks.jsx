b50c1827721caf416b640218c454911f0420f1a6
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-01 00:50:24 +0300

    completed footer

---
+++ frontend/netflix/src/component/Footer/SiteLinks.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './SiteLinks.module.css'
+
+function SiteLinks() {
+  const footerSiteLinks =[
+    "Audio Description",
+    "Investor Relations",
+    "Legal Notices",
+    "Help Center",
+    "Jobs",
+    "Cookie Preferences",
+    "Gift Cards",
+    "Terms of Use",
+    "Corporate INformation",
+    "Media Center",
+    "Privacy",
+    "Contact Us"
+  ]
+
+  return (
+    <div>
+      <ul className={styles.linksList}>
+        {
+          footerSiteLinks.map(link=>{return(
+          <li key={link} className={styles.siteLink}>
+            <a href="/">{link}</a>
+          </li>)})
+        }
+      </ul>
+    </div>
+   
+  )
+}
+
+export default SiteLinks
