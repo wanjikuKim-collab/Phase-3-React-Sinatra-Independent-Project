8b8444eeec0f41079ef09585130e57789e9fb535
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-02 20:54:04 +0300

    updated form with handlechange and handlesubmit

--- frontend/src/component/Row/Review.jsx
+++ frontend/src/component/Row/Review.jsx
@@ -3,72 +3,55 @@ import { Button } from 'react-bootstrap'
 import React,{useState} from 'react'
 
 function Review(){
-    const [ form, setForm ] = useState({})
-    const [ errors, setErrors ] = useState({})
-
-    const setField = (field, value) => {
-        setForm({
-          ...form,
-          [field]: value
-        })
-        // Check and see if errors exist, and remove them from the error object:
-        if ( !!errors[field] ) setErrors({
-            ...errors,
-            [field]: null
-        })
-      }
-
-      const findFormErrors = () => {
-        const { name, food, rating, comment } = form
-        const newErrors = {}
-        // name errors
-        if ( !name || name === '' ) newErrors.name = 'cannot be blank!'
-        else if ( name.length > 30 ) newErrors.name = 'name is too long!'
-        // rating errors
-        if ( !rating || rating > 5 || rating < 1 ) newErrors.rating = 'must assign a rating between 1 and 5!'
-        // comment errors
-        if ( !comment || comment === '' ) newErrors.comment = 'cannot be blank!'
-        else if ( comment.length > 100 ) newErrors.comment = 'comment is too long!'
-    
-        return newErrors
-    }
+    const [ formData, setFormData] = useState({
+      name: "",
+      rating: 0,
+      textarea:""
+    })
 
     const handleSubmit = e => {
         e.preventDefault()
-        // get our new errors
-        const newErrors = findFormErrors()
-        // Conditional logic:
-        if ( Object.keys(newErrors).length > 0 ) {
-          // We got errors!
-          setErrors(newErrors)
-        } else {
-          // No errors! Put any logic here for the form submission!
-          alert('Thank you for your feedback!')
-        }
-      }
+    }
+    
+    function handleChange(e){
+      const key = e.target.id
+      setFormData({
+        ...formData,
+       [key]:e.target.value
+      })
+    }
 
     return(
     <div className='App d-flex flex-column align-items-center'>
         <h1>How was the show?</h1>
-        <Form style={{ width: '300px' }}>
+        <Form style={{ width: '300px' }} onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Label>Name</Form.Label>
+            <Form.Label htmlFor='name'>Name</Form.Label>
             <Form.Control 
-            type='text'
-            onChange={ e => setField('name', e.target.value)}
-            isInvalid={ !!errors.name }
+            id='name' 
+            type='text' 
+            value={formData.name} 
+            onChange={handleChange} 
             />
-            <Form.Control.Feedback type='invalid'>
-             { errors.name }
-            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
-            <Form.Label>Rating</Form.Label>
-            <Form.Control type='number'/>
+            <Form.Label htmlFor='rating'>Rating</Form.Label>
+            <Form.Control 
+            id='rating' 
+            type='number' 
+            value={formData.rating} 
+            onChange={handleChange}
+            />
           </Form.Group>
           <Form.Group>
-            <Form.Label>Comments</Form.Label>
-            <Form.Control as='textarea'/>
+            <Form.Label htmlFor='textarea'>Comments</Form.Label>
+            <Form.Control 
+            id='textarea' 
+            as='textarea' 
+            placeholder='Leave a comment' 
+            value={formData.textarea} 
+            onChange={handleChange}
+            />
           </Form.Group>
           <Button type='submit'>Submit Review</Button>
         </Form>
