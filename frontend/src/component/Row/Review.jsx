b61c796e4a97b20cbfebd4d9d9b061cec83ca98e
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-06 14:50:42 +0300

    made component to render reviews instead of a form

--- frontend/src/component/Row/Review.jsx
+++ frontend/src/component/Row/Review.jsx
@@ -1,62 +1,24 @@
-import Form from 'react-bootstrap/Form'
-import { Button } from 'react-bootstrap'
-import React,{useState} from 'react'
+import React from 'react'
 
-function Review(){
-    const [ formData, setFormData] = useState({
-      name: "",
-      rating: 0,
-      textarea:""
-    })
+function Review({reviews}) {
+  return (
+    <div>
+        <h1>REVIEWS</h1>
+        {reviews.map(review=>(
+            <div class="border-bottom">
+                <p style={{color:'green'}}>{review.viewer.name}</p>
+                <span>{Array(review.ratings).fill("⭐").join("")}</span>
+                <li id={review.id} className='review'>{review.comment} 
+                    <span>
+                        <i className="icon edit"/>
+                        <i className="icon close"/>
+                    </span>
+                </li>                        
 
-    // const handleSubmit = e => {
-    //     e.preventDefault()
-    //     fetch("http://localhost:9292/reviews/", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify(formData)
-    //     })
-    //         .then(r => r.json())
-    //         .then(movie => console.log(movie))
-    // }
-    
-    function handleChange(e){
-      const key = e.target.id
-      setFormData({
-        ...formData,
-       [key]:e.target.value
-      })
-    }
-
-    return(
-    <div className='App d-flex flex-column align-items-center'>
-        <h1>How was the show?</h1>
-        <Form style={{ width: '300px' ,color: "red"}}>
-          <Form.Group>
-            <Form.Label htmlFor='rating'>Rating</Form.Label>
-            <Form.Control 
-            id='rating' 
-            type='number' 
-            value={formData.rating} 
-            onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor='textarea'>Comments</Form.Label>
-            <Form.Control 
-            id='textarea' 
-            as='textarea' 
-            placeholder='Leave a comment' 
-            value={formData.textarea} 
-            onChange={handleChange}
-            />
-          </Form.Group>
-          <Button type='submit'>Submit Review</Button>
-        </Form>
+        </div>   
+                        ))}
     </div>
-    )
+  )
 }
 
 export default Review
